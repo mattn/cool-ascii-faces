65925b9103eb999f96836622fc99267eabeb3168
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-03-04 18:09:40 +0900

    Add -n

--- cmd/faces/main.go
+++ cmd/faces/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/mattn/cool-ascii-faces"
 )
 
+var n = flag.Int("n", 0, "N faces")
+
 func main() {
-	for c := range faces.Stream() {
-		fmt.Println(c)
+	flag.Parse()
+	if *n == 0 {
+		for c := range faces.Stream() {
+			fmt.Println(c)
+		}
+	} else {
+		for i := 0; i < *n; i++ {
+			fmt.Println(faces.String())
+		}
 	}
 }
