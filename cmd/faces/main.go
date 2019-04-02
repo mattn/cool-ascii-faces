08c9c26a685abd2a6eb9e41a3ee941184ea610fa
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-04-02 17:13:46 +0900

    Sleep

--- cmd/faces/main.go
+++ cmd/faces/main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
+
 	"github.com/mattn/cool-ascii-faces"
 )
 
-var n = flag.Int("n", 0, "N faces")
+var (
+	n = flag.Int("n", 0, "N faces")
+	s = flag.Duration("s", 0, "Sleep")
+)
 
 func main() {
 	flag.Parse()
 	if *n == 0 {
 		for c := range faces.Stream() {
 			fmt.Println(c)
+			time.Sleep(*s)
 		}
 	} else {
 		for i := 0; i < *n; i++ {
